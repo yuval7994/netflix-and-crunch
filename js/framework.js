ac3751b77c18a06e3f2c711dc3bdd76bebf85c6f
yuval7994 <yuvalba45@gmail.com>
2022-03-22 18:46:26 -0500

    initial files

---
+++ js/framework.js
@@ -0,0 +1,5 @@
+var startButton = document.getElementById("start");
+var questionEl = document.getElementById("question-title");
+var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var startScreenEl = document.getElementById("start-screen");
---
+++ js/questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Booleans"
+    },
+  
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      answer: "Parenthesis"
+    },
+  
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "Numbers and Strings",
+        "Other Arrays",
+        "Booleans",
+        "All of the Above"
+      ],
+      answer: "All of the Above"
+    },
+  
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    answer: "Curly Brackets"
+  },
+  
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "Terminal / Bash", "For Loops", "console.log"],
+    answer: "for loop"
+  },
+  
+  ];
