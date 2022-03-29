c58a555dd64c9b33bd363195456b4e7b51dda74b
yuval7994 <yuvalba45@gmail.com>
2022-03-28 23:34:50 -0500

    movie api connected

--- js/framework.js
+++ js/framework.js
@@ -1,4 +1,3 @@
-
 var questions = [
     {
         question: "What do you want to eat?",
@@ -23,72 +22,90 @@ var questions = [
 ]
 
 
-questions.forEach(function(question) {
+questions.forEach(function (question) {
     console.log(question.question)
-        for (let i = 0; i < question.answers.length; i++) {
-          const answer = question.answers[i];
-            console.log(answer)
-        }
-    question.answers.forEach(function(answer) {
+    for (let i = 0; i < question.answers.length; i++) {
+        const answer = question.answers[i];
+        console.log(answer)
+    }
+    question.answers.forEach(function (answer) {
         console.log(answer)
     });
 });
 
+/*
+          <div class ="flex-line-1">
+                <span>
+                    <img class="question-1" src = "assets/images/veggies.jpeg">
+                    <h3 class="card-header text-lowercase">Pick a number</h3>
+                </span>
+            </div>  
+*/
 
-request.send(); 
+
+const questionFive = questions[4]
+const questionFiveTitleElement = document.getElementById('question5-title')
+questionFiveTitleElement.innerHTML = questionFive.question
+const questionFiveChoicesElement = document.getElementById('choices-5')
+questionFive.answers.forEach(answer => {
+    const answerContainer = document.createElement('div')
+    answerContainer.className = "flex-line-1"
+    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
+    answerContainer.onclick = () => console.log(getMovieInfo(answer))
+    questionFiveChoicesElement.appendChild(answerContainer)
+}
+)
+
+
+// request.send(); 
 
 //IMDB movie API
-var getMovieInfo = function (movieData) {
+var getMovieInfo = function (movieNumber) {
     var apiMovieUrl = 'https://imdb-api.com/en/API/Top250TVs/k_6day5f3p';
     fetch(
         apiMovieUrl
     )
-        .then(function(movieResponse) {
+        .then(function (movieResponse) {
             return movieResponse.json();
         })
         .then(function (movieResponse) {
-            console.log(movieResponse)
-            var rank = movieResponse;
+            console.log(movieResponse.items)
+            var movie = movieResponse.items.find(movie => movie.rank === movieNumber)
 
-            var movie = movieResponse;
-            
-            return fetch('https://imdb-api.com/en/API/Top250TVs/k_6day5f3p')
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function(response) {
-           console.log(response);
-           displayMovie(response);
-       });
-     
+            console.log(movie);
 
- };
+
+
+        })
+
+
+};
+
 // Food Api
- var getfoodInfo = function (foodData) {
+var getfoodInfo = function (foodData) {
     var apifoodUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
     fetch(
         apifoodUrl
     )
-        .then(function(foodResponse) {
-           return foodResponse.json();
-       })
+        .then(function (foodResponse) {
+            return foodResponse.json();
+        })
         .then(function (foodResponse) {
             console.log(foodResponse)
             var rank = foodResponse;
 
             var food = foodResponse;
-            
+
             return fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         })
-       .then(function (response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(response) {
-           console.log(response);
-           displayfood(response);
+        .then(function (response) {
+            console.log(response);
+            displayfood(response);
         });
-     
+
 
 };
 
