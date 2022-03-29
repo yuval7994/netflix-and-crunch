7daa5565e22a498c2b0ab4f3e84fc197291c3544
Emily Arnholt <emilynarnholt@gmail.com>
2022-03-28 20:37:06 -0500

    framework

--- js/framework.js
+++ js/framework.js
@@ -10,7 +10,7 @@ var questions = [
     },
     {
         question: "What category do you enjoy the most?",
-        answers: ["Pork", "Pork", "Lamb", "Breakfast"]
+        answers: ["Pork", "Pasta", "Lamb", "Breakfast"]
     },
     {
         question: "What are you in the mood for?",
@@ -25,31 +25,73 @@ var questions = [
 
 questions.forEach(function(question) {
     console.log(question.question)
-    // for (let i = 0; i < question.answers.length; i++) {
-    //     const answer = question.answers[i];
-    //     console.log(answer)
-    // }
+        for (let i = 0; i < question.answers.length; i++) {
+          const answer = question.answers[i];
+            console.log(answer)
+        }
     question.answers.forEach(function(answer) {
         console.log(answer)
     });
 });
 
-function getApi() {
-    var requestUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=68846c14fbb9ad592255787949832875&language=en-US&page=1';
-    fetch(requestUrl) 
-        .then(function(response) {
-          return response.json();
-        });
-    
-    }
-    getApi()
 
-function getApi() {
-    var requestUrl = 'https://themealdb.com/api/json/v1/1/random.php'; 
-    fetch(requestUrl) 
-      .then(function(response) {
-        return response.json();
-      });
-  
-  }
-  getApi()
+request.send(); 
+
+//IMDB movie API
+var getMovieInfo = function (movieData) {
+    var apiMovieUrl = 'https://imdb-api.com/en/API/Top250TVs/k_6day5f3p';
+    fetch(
+        apiMovieUrl
+    )
+        .then(function(movieResponse) {
+            return movieResponse.json();
+        })
+        .then(function (movieResponse) {
+            console.log(movieResponse)
+            var rank = movieResponse;
+
+            var movie = movieResponse;
+            
+            return fetch('https://imdb-api.com/en/API/Top250TVs/k_6day5f3p')
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(response) {
+           console.log(response);
+           displayMovie(response);
+       });
+     
+
+ };
+// Food Api
+ var getfoodInfo = function (foodData) {
+    var apifoodUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    fetch(
+        apifoodUrl
+    )
+        .then(function(foodResponse) {
+           return foodResponse.json();
+       })
+        .then(function (foodResponse) {
+            console.log(foodResponse)
+            var rank = foodResponse;
+
+            var food = foodResponse;
+            
+            return fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        })
+       .then(function (response) {
+            return response.json();
+        })
+        .then(function(response) {
+           console.log(response);
+           displayfood(response);
+        });
+     
+
+};
+
+
+
+
