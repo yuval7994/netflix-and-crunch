c913bf565451b6faeb39d82f786991aeb3f86281
yuval7994 <yuvalba45@gmail.com>
2022-03-29 18:06:11 -0500

    added food api

--- js/framework.js
+++ js/framework.js
@@ -8,13 +8,10 @@ var questions = [
         answers: ["Chinese", "American", "Italian", "Spanish"]
     },
     {
-        question: "What category do you enjoy the most?",
-        answers: ["Pork", "Pasta", "Lamb", "Breakfast"]
+        question: "Which ingredient do you enjoy the most?",
+        answers: ["Sugar", "Garlic", "Lemon", "Spaghetti"]
     },
-    {
-        question: "What are you in the mood for?",
-        answers: ["Low calorie", "Meat", "Fusion", "Soup"]
-    },
+
     {
         question: "Pick a number",
         answers: ["213", "177", "25", "3"]
@@ -41,7 +38,7 @@ questions.forEach(function (question) {
                 </span>
             </div>  
 */
-const questionOne = questions[4]
+const questionOne = questions[0]
 const questionOneTitleElement = document.getElementById('question1-title')
 questionOneTitleElement.innerHTML = questionOne.question
 const questionOneChoicesElement = document.getElementById('choices-1')
@@ -53,7 +50,7 @@ questionOne.answers.forEach(answer => {
     questionOneChoicesElement.appendChild(answerContainer)
 }
 )
-const questionTwo = questions[4]
+const questionTwo = questions[1]
 const questionTwoTitleElement = document.getElementById('question2-title')
 questionTwoTitleElement.innerHTML = questionTwo.question
 const questionTwoChoicesElement = document.getElementById('choices-2')
@@ -65,7 +62,7 @@ questionTwo.answers.forEach(answer => {
     questionTwoChoicesElement.appendChild(answerContainer)
 }
 )
-const questionThree = questions[4]
+const questionThree = questions[2]
 const questionThreeTitleElement = document.getElementById('question3-title')
 questionThreeTitleElement.innerHTML = questionThree.question
 const questionThreeChoicesElement = document.getElementById('choices-3')
@@ -76,30 +73,19 @@ questionThree.answers.forEach(answer => {
     answerContainer.onclick 
     questionThreeChoicesElement.appendChild(answerContainer)
 }
+
 )
-const questionFour = questions[4]
+
+const questionFour = questions[3]
 const questionFourTitleElement = document.getElementById('question4-title')
-questionFourTitleElement.innerHTML = questionFive.question
+questionFourTitleElement.innerHTML = questionFour.question
 const questionFourChoicesElement = document.getElementById('choices-4')
 questionFour.answers.forEach(answer => {
     const answerContainer = document.createElement('div')
     answerContainer.className = "flex-line-1"
     answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
-    answerContainer.onclick = 
-    questionFourChoicesElement.appendChild(answerContainer)
-}
-)
-
-const questionFive = questions[4]
-const questionFiveTitleElement = document.getElementById('question5-title')
-questionFiveTitleElement.innerHTML = questionFive.question
-const questionFiveChoicesElement = document.getElementById('choices-5')
-questionFive.answers.forEach(answer => {
-    const answerContainer = document.createElement('div')
-    answerContainer.className = "flex-line-1"
-    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
     answerContainer.onclick = () => console.log(getMovieInfo(answer))
-    questionFiveChoicesElement.appendChild(answerContainer)
+    questionFourChoicesElement.appendChild(answerContainer)
 }
 )
 
@@ -129,33 +115,23 @@ var getMovieInfo = function (movieNumber) {
 };
 
 // Food Api
-var getfoodInfo = function (foodData) {
-    var apifoodUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+var getfoodInfo = function (category, area, ingredient) {
+    var apifoodUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}&a=${area}&i=${ingredient}`;
     fetch(
         apifoodUrl
     )
         .then(function (foodResponse) {
             return foodResponse.json();
-        })
-        .then(function (foodResponse) {
-            console.log(foodResponse)
-            var rank = foodResponse;
 
-            var food = foodResponse;
-
-            return fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
-            displayfood(response);
-        });
+      .then(function (foodData) {
+          console.log(foodData)
+          
 
+      })
 
 };
-
+getfoodInfo('Seafood', 'Italian', 'Spaghetti')
 
 
 
