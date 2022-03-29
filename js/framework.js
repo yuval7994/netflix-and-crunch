3fb61b811ad9b3b9b89e4ecefc53c0109d19cbd5
yuval7994 <yuvalba45@gmail.com>
2022-03-29 11:59:56 -0500

    edited questions

--- js/framework.js
+++ js/framework.js
@@ -41,7 +41,54 @@ questions.forEach(function (question) {
                 </span>
             </div>  
 */
-
+const questionOne = questions[4]
+const questionOneTitleElement = document.getElementById('question1-title')
+questionOneTitleElement.innerHTML = questionOne.question
+const questionOneChoicesElement = document.getElementById('choices-1')
+questionOne.answers.forEach(answer => {
+    const answerContainer = document.createElement('div')
+    answerContainer.className = "flex-line-1"
+    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
+    answerContainer.onclick 
+    questionOneChoicesElement.appendChild(answerContainer)
+}
+)
+const questionTwo = questions[4]
+const questionTwoTitleElement = document.getElementById('question2-title')
+questionTwoTitleElement.innerHTML = questionTwo.question
+const questionTwoChoicesElement = document.getElementById('choices-2')
+questionTwo.answers.forEach(answer => {
+    const answerContainer = document.createElement('div')
+    answerContainer.className = "flex-line-1"
+    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
+    answerContainer.onclick 
+    questionTwoChoicesElement.appendChild(answerContainer)
+}
+)
+const questionThree = questions[4]
+const questionThreeTitleElement = document.getElementById('question3-title')
+questionThreeTitleElement.innerHTML = questionThree.question
+const questionThreeChoicesElement = document.getElementById('choices-3')
+questionThree.answers.forEach(answer => {
+    const answerContainer = document.createElement('div')
+    answerContainer.className = "flex-line-1"
+    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
+    answerContainer.onclick 
+    questionThreeChoicesElement.appendChild(answerContainer)
+}
+)
+const questionFour = questions[4]
+const questionFourTitleElement = document.getElementById('question4-title')
+questionFourTitleElement.innerHTML = questionFive.question
+const questionFourChoicesElement = document.getElementById('choices-4')
+questionFour.answers.forEach(answer => {
+    const answerContainer = document.createElement('div')
+    answerContainer.className = "flex-line-1"
+    answerContainer.innerHTML = `<h3 class="card-header text-lowercase">${answer}</h3>`
+    answerContainer.onclick = 
+    questionFourChoicesElement.appendChild(answerContainer)
+}
+)
 
 const questionFive = questions[4]
 const questionFiveTitleElement = document.getElementById('question5-title')
